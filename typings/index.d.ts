e9ff3878abf85752ccb8ac188a132012caad00b8
Trung Nguyen <ttrung2409@gmail.com>
2017-01-03 21:12:40 +0700

    add reame and remove redundant code

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/isomorphic-fetch/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/redux-thunk/index.d.ts" />
-/// <reference path="globals/redux/index.d.ts" />
